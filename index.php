56302e36669b72a81663bfc05bb5d183f34f088d
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-06-28 17:45:26 +0300

    update main page

--- index.php
+++ index.php
@@ -13,47 +13,85 @@
  * @package i3d
  */
 
-wp_redirect(esc_url(get_post_type_archive_link('icon')));
-exit;
+// wp_redirect(esc_url(get_post_type_archive_link('icon')));
+// exit;
 get_header();
 ?>
 
-<main id="primary" class="site-main">
+<?php
+$paged = (get_query_var('page')) ? get_query_var('page') : 1;
+$i3d_post_type = $_GET['post_type'];
+if (!$i3d_post_type) {
+	$i3d_post_type = 'icon';
+}
+$args = array(
+	'post_type'      => $i3d_post_type,
+	'orderby'        => 'date',
+	'order'          => 'DESC',
+	'paged' => $paged
+);
+$q = new WP_Query($args);
+$temp_query = $wp_query;
+$wp_query   = NULL;
+$wp_query   = $q;
+?>
 
-	<?php
-	if (have_posts()) :
+<div class="container">
+	<div class="background video-background">
+		<div class="video-background__el">
+			<video class="video-background__video js-video js-video-no-loop" src="<?php echo get_stylesheet_directory_uri(); ?>/app/video/Main-screen.mp4" autoplay="autoplay" muted="muted"></video>
+		</div>
+	</div>
+	<section class="hello-screen">
+		<div class="hello-screen__wrap wrap">
+			<div class="hello-screen__inner">
+				<div class="hello-screen__content content content_gray h5">
+					<h1>
+						All <span class="color-blue">3D </span>icons you need in one
+						place
+					</h1>
+					<p>
+						This is a collection of free, beautiful, trending 3D icons, that
+						you can use <br />
+						in your website, app or in any project. Search and click to
+						download.
+					</p>
+				</div>
+				<?php echo get_search_form(); ?>
+				<div class="hello-screen__bottom">
+					<div class="hello-screen__switcher switcher">
+						<a href="<?php echo home_url() ?>?post_type=icons" class="switcher__link is-active">Icons</a>
+						<a href="<?php echo home_url() ?>?post_type=packs" class="switcher__link">Icon packs</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-		if (is_home() && !is_front_page()) :
-	?>
-			<header>
-				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-			</header>
-	<?php
-		endif;
+	<section class="icons-list icons-list--icons">
+		<div class="icons-list__wrap wrap">
+			<?php if (have_posts()) : ?>
+				<div class="icons-list__row flex">
+					<?php while (have_posts()) : ?>
+						<?php
+						the_post();
+						get_template_part('template-parts/content', get_post_type());
+						?>
+					<?php endwhile; ?>
+				</div>
+				<div class="icons-list__bottom">
+					<?php echo i3d_custom_pagination(); ?>
+				</div>
+			<?php else : ?>
+				<?php get_template_part('template-parts/content', 'none'); ?>
+			<?php endif; ?>
+		</div>
+	</section>
 
-		/* Start the Loop */
-		while (have_posts()) :
-			the_post();
 
-			/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-			get_template_part('template-parts/content', get_post_type());
+	<?php echo do_shortcode('[trending type="packs"]'); ?>
 
-		endwhile;
-
-		the_posts_navigation();
-
-	else :
-
-		get_template_part('template-parts/content', 'none');
-
-	endif;
-	?>
-
-</main><!-- #main -->
+</div>
 
 <?php
 get_footer();
